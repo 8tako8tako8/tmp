de79dd1591eb510263c9a67ece70544bed01935c
8tako8tako8 <8tako8tako8@gmail.com>
2023-12-29 11:06:10 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src="/comeback_basketball_icon.png"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
